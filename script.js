85c9f63f8ba753263dc48f39d85e3c59d30e621e
Mahabubur Rahman <microronyy@gmail.com>
2021-09-28 12:24:54 +0600

    add swiper

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  observer: true,
+  observeParents: true,
+});
