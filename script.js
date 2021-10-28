ec0ad655b0a36c26208f9c37c57ede20addff63d
Mahabubur Rahman <microronyy@gmail.com>
2021-10-28 18:02:41 +0600

    faq page contact page explore page

--- script.js
+++ script.js
@@ -99,29 +99,96 @@ var swiper = new Swiper('.about-page-swiper', {
   },
 });
 
-(function(){
-	const galleryTabsList = document.querySelector('.gallery-tabs-list');
-	const galleryTabs = document.querySelectorAll('.gallery-tab');
-	const galleryContents = document.querySelectorAll('.gallery-tab-content');
+(function () {
+  const galleryTab = () => {
+    const galleryTabsList = document.querySelector('.gallery-tabs-list');
+    const galleryTabs = document.querySelectorAll('.gallery-tab');
+    const galleryContents = document.querySelectorAll('.gallery-tab-content');
 
-	galleryTabsList.addEventListener('click', toggleTabs);
+    if (galleryTabsList && galleryTabs && galleryContents) {
+      galleryTabsList.addEventListener('click', toggleTabs);
 
-  function toggleTabs(e) {
-		if (e.target && e.target.nodeName === 'LI') {
-			//change tabs
-		  for (let i = 0; i < galleryTabs.length; i++) {
-				galleryTabs[i].classList.remove('active');
-			}
-			e.target.classList.toggle('active');
+      function toggleTabs(e) {
+        if (e.target && e.target.nodeName === 'LI') {
+          //change tabs
+          for (let i = 0; i < galleryTabs.length; i++) {
+            galleryTabs[i].classList.remove('active');
+          }
+          e.target.classList.toggle('active');
 
-			//change contents
-			for (let i = 0; i < galleryContents.length; i++) {
-				galleryContents[i].classList.remove('active');
-			}
+          //change contents
+          for (let i = 0; i < galleryContents.length; i++) {
+            galleryContents[i].classList.remove('active');
+          }
 
-			const tabID = `#${e.target.dataset.tabId}`;
+          const tabID = `#${e.target.dataset.tabId}`;
 
-			document.querySelector(tabID).classList.toggle('active');
-		}
-	}
+          document.querySelector(tabID).classList.toggle('active');
+        }
+      }
+    }
+  };
+
+  galleryTab();
+
+  const galleryModal = () => {
+		
+    const galleryModals = document.querySelectorAll('#gallery-modal-btn');
+    const galleryCloseButtons = document.querySelectorAll('#close-btn');
+    const galleryModalOverlays = document.querySelectorAll(
+      '#gallery-modal-overlay'
+    );
+
+
+    galleryModals.map(element => {
+      element.addEventListener('click', function () {
+        document
+          .getElementById('gallery-modal-overlay')
+          .classList.add('is-visible');
+        document.getElementById('gallery-modal').classList.add('is-visible');
+      });
+    });
+
+    galleryCloseButtons.map(element => {
+      element.addEventListener('click', function () {
+        document
+          .getElementById('gallery-modal-overlay')
+          .classList.remove('is-visible');
+        document.getElementById('gallery-modal').classList.remove('is-visible');
+      });
+    });
+
+    galleryModalOverlays.map(element => {
+      element.addEventListener('click', function () {
+        document
+          .getElementById('gallery-modal-overlay')
+          .classList.remove('is-visible');
+        document.getElementById('gallery-modal').classList.remove('is-visible');
+      });
+    });
+  };
+
+  // galleryModal();
+
+  const faqAccordion = () => {
+    var accItem = document.getElementsByClassName('accordionItem');
+    var accHD = document.getElementsByClassName('accordionItemHeading');
+
+    if (accItem && accHD) {
+      for (let i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+      }
+      function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (let i = 0; i < accItem.length; i++) {
+          accItem[i].className = 'accordionItem close';
+        }
+        if (itemClass == 'accordionItem close') {
+          this.parentNode.className = 'accordionItem open';
+        }
+      }
+    }
+  };
+
+  faqAccordion();
 })();
